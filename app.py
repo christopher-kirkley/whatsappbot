891c8a5a52f5b7f501d571b85c90c9d9345d1b12
Christopher Kirkley <ckirkley@gmail.com>
2021-06-28 15:54:57 -0700

    Clean up code

--- app.py
+++ app.py
@@ -17,31 +17,9 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    # if 'listen' in incoming_msg:
-    #     msg.body("What would you like to listen to?\n" +
-    #             "1: Etran de L'Air\n" +
-    #             "2: Oumou Diabate & Kara Show\n" + 
-    #             "3: Alkibar Gignor\n")
-    #     responded = True
     if 'song' in incoming_msg:
         msg.media(random.choice(songs))
         responded = True
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
-    # if not responded:
-    #     msg.body('I only know about famous quotes and cats, sorry!')
     return str(resp)
 
 
